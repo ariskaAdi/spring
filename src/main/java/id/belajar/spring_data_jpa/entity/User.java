9f194f48e5f1d1526bd99e6e9eeb8532d614e2bf
ariskaAdi <ariskaadiprasetyo@gmail.com>
2026-03-05 11:41:23 +0700

    first commit

---
+++ src/main/java/id/belajar/spring_data_jpa/entity/User.java
@@ -0,0 +1,36 @@
+package id.belajar.spring_data_jpa.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    private String username;
+
+    private String password;
+
+    private String name;
+
+    private String token;
+
+    @Column(name = "token_expred_all")
+    private Long tokenExpired;
+
+    @OneToMany(mappedBy = "user")
+    private List<Contact> contacts;
+
+
+}
